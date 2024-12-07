116befafbf139d41c3ceac581aa13b909ed5a1ee
joe-bring <3403027828@qq.com>
2024-12-07 21:49:08 +0800

    Update content.js

--- chrome_extention/js/content.js
+++ chrome_extention/js/content.js
@@ -9,42 +9,7 @@ function injectJs(href, callback){
 injectJs(chrome.runtime.getURL("js/hook.js"));
 
 
-var cachedApiUrl = "http://127.0.0.1:8485";
-
-chrome.storage.sync.get('apiUrl', function(result) {
-    if (result.apiUrl) {
-        cachedApiUrl = result.apiUrl;
-        console.log("API URL cached:", cachedApiUrl);
-    } else {
-        console.log("No API URL found in storage");
-    }
-});
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    if (changes.apiUrl) {
-        cachedApiUrl = changes.apiUrl.newValue;
-        console.log("API URL updated in cache:", cachedApiUrl);
-    }
-});
 
 window.addEventListener("sendToAPI", async Ot => {
-     if (cachedApiUrl) {
-        fetch(cachedApiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({data: Ot.detail})
-        })
-        .then(response => response.json())
-        .then(responseData => {
-            console.log("Response from API:", responseData);
-        })
-        .catch(error => {
-            console.error("Error during API request:", error);
-        });
-    }
-    else {
-        console.error("No API URL cached");
-    }
+    chrome.runtime.sendMessage({ type: "SEND_HTTP", data: Ot.detail});
 })
